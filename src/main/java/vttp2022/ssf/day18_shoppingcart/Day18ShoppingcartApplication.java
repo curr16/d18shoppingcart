c6c7949dec4227c215d4a85051cae31229c639ea
curlynn <curlynntan@yahoo.com.sg>
2022-08-04 14:17:26 +0800

    first commit

---
+++ src/main/java/vttp2022/ssf/day18_shoppingcart/Day18ShoppingcartApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.day18_shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day18ShoppingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day18ShoppingcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/ssf/day18_shoppingcart/Day18ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.day18_shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day18ShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
